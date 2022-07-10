8680ce6c77e828536019bc832c1ad165b8a406b9
Meghanath Shetty <meghanathshetty108@gmail.com>
2022-07-10 19:08:05 +0530

    Full optimiszd code

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,20 +1,47 @@
-console.log("working")
-let keys = document.querySelectorAll(".key");
-let audios = document.querySelectorAll("audio")
-console.log(keys[1].dataset.key)
 
-window.addEventListener("keydown",(e)=>{
-    let code = `${e.keyCode}`;
-    console.log(code);
-    keys.forEach((item,idx)=>{
-        let key = item.dataset.key;
-        item.classList.remove("playing");
-        if(key === code){
-            item.classList.add("playing");
-            audios[idx].play();
-            console.log(item.classList)
-        } 
+  window.addEventListener("keydown",(e)=>{ //listen window for keydown
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);//select the audio tag which matches with keydown code and its attribute code
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //if no audio tag found then simply return
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+  })
 
-    })
 
-})
+  
+  function removeTransition(e){
+    if(e.propertyName !=="transform") return;
+    this.classList.remove("playing");
+  }
+
+  const keys = document.querySelectorAll(".key");
+  keys.forEach(key=>{
+    key.addEventListener("transitionend",removeTransition);
+  })
+
+
+
+
+
+//First Try Code non optimised
+
+// let keys = document.querySelectorAll(".key");
+// let audios = document.querySelectorAll("audio")
+
+
+// window.addEventListener("keydown",(e)=>{
+//     let code = `${e.keyCode}`;
+//     console.log(code);
+//     keys.forEach((item,idx)=>{
+//         let key = item.dataset.key;
+//         item.classList.remove("playing");
+//         if(key === code){
+//             item.classList.add("playing");
+//             audios[idx].currentTime=0;
+//             audios[idx].play();
+//         } 
+
+//     })
+
+// })
